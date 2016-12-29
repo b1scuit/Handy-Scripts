81a46c4c4ffb7300b4b9191bdce62a70dd57172d
Chicken Biscuit <dysvir@mail.ru>
2016-12-29 22:52:51 +0000

    modified:   misc/PHP-obj-arr.php

--- misc/PHP-obj-arr.php
+++ misc/PHP-obj-arr.php
@@ -1,6 +1,6 @@
 <?Php
 
-// The class
+// The class, you can have others like users, articals, guests etc..
 
 class Animal {
 
@@ -22,15 +22,29 @@ class Animal {
 }
 
 // The array of what your classes will be based on
+// This can be from MySQL or a database
 
 $arrayOfAnimals =
 [
-  'Cow' => ['legs' => 4, 'eyes' => 2],
-  'Duck' => ['legs' => 2, 'eyes' => 2],
-  'Dog' => ['legs' => 4, 'eyes' => 2],
-  'Spider' => ['legs' => 8, 'eyes' => 8]
+  'Cow' => [
+    'legs' => 4,
+    'eyes' => 2
+  ],
+  'Duck' => [
+    'legs' => 2,
+    'eyes' => 2
+  ],
+  'Dog' => [
+    'legs' => 4,
+    'eyes' => 2
+  ],
+  'Spider' => [
+    'legs' => 8,
+    'eyes' => 8
+  ]
 ];
 
+// Empty array initalized
 $arrayOfObjects = [];
 
 // Turning the array into instances of the class
@@ -46,12 +60,13 @@ foreach ($arrayOfAnimals as $key => $value){
 
 }
 
-// What you would use this for
+// What you would use this for (List users, articals, comments etc..)
 
 foreach($arrayOfObjects as $object){
   echo '<p>A ' . $object->name . ' has ' . $object->noOfEyes . ' eyes and ' . $object->noOfLegs . ' legs.</p>';
 }
 
+// Using this to echo a perticular object's value from the array
 echo $arrayOfObjects['Cow']->noOfLegs;
 
 ?>
