ad265dd2f9e791cffba0a35c9a25630023493b1a
Chicken Biscuit <dysvir@mail.ru>
2016-12-30 14:20:15 +0000

    modified:   misc/PHP-obj-arr-2.php

--- misc/PHP-obj-arr-2.php
+++ misc/PHP-obj-arr-2.php
@@ -9,7 +9,8 @@ USE `testDB` ;
 CREATE TABLE IF NOT EXISTS `testDB`.`users` (
 `id` INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
 `username` VARCHAR(45) NULL DEFAULT NULL,
-`full_name` VARCHAR(45) NULL DEFAULT NULL,
+`firstname` VARCHAR(45) NULL DEFAULT NULL,
+`surname` VARCHAR(45) NULL DEFAULT NULL,
 `role` VARCHAR(45) NULL DEFAULT NULL);
 
 INSERT INTO `testDB`.`users` (`id`, `username`, `firstname`, `surname`, `role`) VALUES (DEFAULT, 'Person1', 'Person', 'One', 'Admin');
