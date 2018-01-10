6a6b2b617f101815045698d2ee1637afdbffe87f
Chicken Biscuit <dysvir@gmail.com>
2018-01-10 11:06:06 +0000

    Added MySQL DIr Python package

--- Python/MySQLDir/setup.py
+++ Python/MySQLDir/setup.py
@@ -9,11 +9,11 @@ with open('LICENSE') as f:
 
 setup(
     name='mysqlDir',
-    version='0.1.0',
-    description='Sample package for Python-Guide.org',
+    version='0.0.1',
+    description='Sample package example',
     long_description=readme,
-    author='Kenneth Reitz',
-    author_email='me@kennethreitz.com',
+    author='B1scuit',
+    author_email='dysvir@gmail.com',
     url='https://github.com/B1scuit/Python/MySQLDir',
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
