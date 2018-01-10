20f76d2b53275df46f204a2cbffb9c91d9f76eac
Chicken Biscuit <dysvir@gmail.com>
2018-01-10 10:28:58 +0000

    Reordered things a bit

--- Security-Practicals/getAction.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
- <head>
-  <meta charset="UTF-8">
-  <title>ECHO GET</title>
- </head>
- <body>
-  <?php echo 'Hello ' . $_GET["firstname"]; ?>
- </body>
-</html>
--- Security-Practicals/postAction.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
- <head>
-  <meta charset="UTF-8">
-  <title>ECHO POST</title> 
- </head>
- <body>
-  <?php echo 'Hello ' . $_POST["firstname"]; ?>
- </body>
-</html>
