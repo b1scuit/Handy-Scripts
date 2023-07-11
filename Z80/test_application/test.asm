e73fe0dbaf71b7759e654003e95550a36b3d4962
Peter Holt <peter.holt@dochq.co.uk>
2023-07-11 23:58:05 +0100

    added devops

--- Z80/test_application/test.asm
+++ Z80/test_application/test.asm
@@ -1,10 +1,9 @@
 LED_ADDR    .EQU $0000
-LED_INIT    .EQU $02; INIT 4-bit mode
-LED_4BIT    .EQU $28; Initialization of 16X2 LCD in 4bit mode
-LED_ENABLE  .EQU $0E; Enable Display, Disable Cursor
-LED_INCCUR  .EQU $06; Auto Increment cursor
+LED_INIT    .EQU $14; Function Set
+LED_ENABLE  .EQU $0F; Display ON/Off
+LED_INCCUR  .EQU $07; Entry Mode eset
 LED_CLRDIS  .EQU $01; Clear Display
-LED_CURHOM  .EQU $80; Cursor at home position
+LED_CURHOM  .EQU $02; Return home
 
     .ORG $0000
 RST00:
@@ -16,8 +15,6 @@ INIT:
     LD HL,$80ED
     LD SP,HL
     LD A,LED_INIT
-    CALL SENDCMD
-    LD A,LED_4BIT
     CALL SENDCMD
     LD A,LED_ENABLE
     CALL SENDCMD
