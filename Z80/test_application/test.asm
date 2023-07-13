e760768ade5e5d68ee6d8f1e1e75aae8871e3ca9
B1scuit <dysvir@gmail.com>
2023-07-13 12:02:28 +0100

    Tweaks

--- Z80/test_application/test.asm
+++ Z80/test_application/test.asm
@@ -1,10 +1,13 @@
 led_addr        .equ $0000
-led_init        .equ $14; Function Set
-led_enable      .equ $0f; Display ON/OFF
-led_inccur      .equ $07; Entry Mode eset
-led_clrdis      .equ $01; Clear Display
-led_curhom      .equ $02; Return home
-led_display_off .equ $08
+
+led_clrdis      .equ $01; Clear Display     0 0 0 0 0 0 0 1
+led_curhom      .equ $02; Return home       0 0 0 0 0 0 1 0
+led_inccur      .equ $07; Entry Mode Set    0 0 0 0 0 1 I S
+led_enable      .equ $0f; Display ON/OFF:   0 0 0 0 1 D C B 
+led_cur         .equ $10; Cursor or Display 0 0 0 1 C D - -
+led_init        .equ $14; Function Set:     0 0 1 DL N F- -
+led_set_cgram   .equ $40; Set CGRam addr    0 1 X X X X X X
+led_set_ddram   .equ $80; Set DDRam addr    1 B X X X X X X
 
     .org $0000
 rst00:
@@ -16,6 +19,18 @@ init:
     ld hl,$80ed ; Sort out the stack pointer
     ld sp,hl
 
+    call init_2400h
+
+    ld   hl,intro
+    ld   b,(hl)
+loop:
+    inc  hl
+    ld   a,(hl)
+    call senddata
+    djnz loop
+end:
+    halt
+init_splc780d:
     ; For some reason we have to send $30 x3 times
     ld a,$30
     call sendcmd
@@ -41,16 +56,17 @@ init:
 
     ld a,$0f
     call sendcmd
+    ret
 
-    ld   hl,intro
-    ld   b,(hl)
-loop:
-    inc  hl
-    ld   a,(hl)
-    call senddata
-    djnz loop
-end:
-    halt
+init_2400h:
+    ld a, led_init
+    call sendcmd
+    ld a,led_enable
+    call sendcmd
+    ld a, led_clrdis
+    call sendcmd
+    ld a, led_curhom
+    ret
 
 sendcmd:
     push af
@@ -64,8 +80,6 @@ sendcmd:
     nop
     ; Send Lower Mibble (4 bytes last)
     pop af
-    rla ; Rotate left then right to knock the 8th bit as 0
-    rra
     res 7,a
     out (led_addr), a
     ret
@@ -81,10 +95,8 @@ senddata:
     nop
     ; Send Lower Mibble (4 bytes last)
     pop af
-    rla ; Rotate left then right to knock the 8th bit as 0
-    rra
     set 7, a
     out (led_addr), a ; Send command to LCD screen
     ret
 
-intro: .byte  16,"Hello Z80 World!"
+intro: .db  16,"Hello Z80 World!"
