250f19406187831a17445f0aea662199032c76ec
B1scuit <dysvir@gmail.com>
2023-07-13 10:51:23 +0100

    THing

--- Z80/test_application/test.asm
+++ Z80/test_application/test.asm
@@ -1,10 +1,11 @@
-LED_ADDR    .EQU $0000
-LED_INIT    .EQU $14; Function Set
-LED_ENABLE  .EQU $0F; Display ON/Off
-LED_INCCUR  .EQU $07; Entry Mode eset
-LED_CLRDIS  .EQU $01; Clear Display
-LED_CURHOM  .EQU $02; Return home
+LED_ADDR        .EQU $0000
+LED_INIT        .EQU $14; Function Set
+LED_ENABLE      .EQU $0F; Display ON/Off
+LED_INCCUR      .EQU $07; Entry Mode eset
+LED_CLRDIS      .EQU $01; Clear Display
+LED_CURHOM      .EQU $02; Return home
 LED_DISPLAY_OFF .EQU $08
+LED_INIT_
 
     .ORG $0000
 RST00:
@@ -13,33 +14,41 @@ RST00:
 
     .ORG $0040 ; Safe place to start
 INIT:
-    LD HL,$80ED
+    LD HL,$80ED ; Sort out the stack pointer
     LD SP,HL
 
-    LD A,$30
-    CALL SENDCMD
-    LD A,$30
-    CALL SENDCMD
-    LD A,$30
-    CALL SENDCMD
-    LD A,$20
-    CALL SENDCMD
-    LD A, $28
-    CALL SENDCMD
+; ; For some reason we have to send $30 x3 times
+; LD A,$30
+; CALL SENDCMD
+; LD A,$30
+; CALL SENDCMD
+; LD A,$30
+; CALL SENDCMD
+; LD A,$20
+; CALL SENDCMD
+; LD A, $28
+; CALL SENDCMD
 
-    LD A, LED_DISPLAY_OFF
-    CALL SENDCMD
+; LD A, LED_DISPLAY_OFF
+; CALL SENDCMD
 
-    LD A, LED_CLRDIS
-    CALL SENDCMD
+; LD A, LED_CLRDIS
+; CALL SENDCMD
 
-    LD A,$06
-    CALL SENDCMD
+; LD A,$06
+; CALL SENDCMD
+; LD A,$02
+; CALL SENDCMD
+
+; LD A,$0F
+; CALL SENDCMD
+
     LD A,$02
-    CALL SENDCMD
-
-    LD A,$0F
-    CALL SENDCMD
+    LD A,$28
+    LD A,$0E
+    LS A,LED_INCCUR
+    LD A,LED_CLRDIS
+    LD A,$80
 
     LD   HL,INTRO
     LD   B,(HL)
